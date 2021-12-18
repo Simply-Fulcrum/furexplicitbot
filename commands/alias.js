7511459a4f3b643dd8c8239717b32c602a990eca
Simply-Fulcrum <lukekerne@gmail.com>
2021-12-18 21:22:25 +1300

    removed fa

--- commands/alias.js
+++ commands/alias.js
@@ -5,8 +5,7 @@ module.exports.run = async (client, message, args, config, MessageEmbed, message
     .setColor(message.member.displayColor)
     .addField('e621', 'e, e6, e62, e621')
     .addField('InkBunny', 'ib, ibun, ibunny, inkb, inkbunny')
-    .addField('Rule34', 'r, 34, r34, ru34, rule34')
-    .addField('FurAffinity', 'f, fa, fura, furaffinity');
+    .addField('Rule34', 'r, 34, r34, ru34, rule34');
   message.channel.send({ embed });
 };
 
